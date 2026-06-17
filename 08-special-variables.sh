0e1571ef6cdfc05e4af34be4197da15336ab10ea
Vamshi Koppolu <vamshi.kkoppolu@gmail.com>
2026-06-17 11:01:21 +0530

    variables

--- 08-special-variables.sh
+++ 08-special-variables.sh
@@ -3,3 +3,6 @@
 echo "All Variables $@"
 echo "All Variables $*"
 echo "Script name: $0"
+echo "Present working directory: $PWD "
+echo "who is running the script: $USER"
+echo "user home directory: $HOME"
