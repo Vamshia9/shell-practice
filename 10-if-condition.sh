c240f2f080c5a43673677a076831000fcabc952e
Vamshi Koppolu <vamshi.kkoppolu@gmail.com>
2026-06-17 15:38:14 +0530

    variables

--- 10-if-condition.sh
+++ 10-if-condition.sh
@@ -2,10 +2,10 @@
 
 Number=$1
 
-if [%Number -lt 10] then
+if [$Number -lt 10] then
     echo "Number is less than 10"
 
-elif [%Number -eq 10] then
+elif [$Number -eq 10] then
     echo "Number is equal to 10"
 else 
     echo "Number is greater than 10"
