8cbf6a0944388df2717e71e486d23eae701a5af4
Vamshi Koppolu <vamshi.kkoppolu@gmail.com>
2026-06-18 17:24:36 +0530

    12-installation

--- 12-installations.sh
+++ 12-installations.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-USERID=$(id -u)
-
-if [ [$USERID] -ne 0 ]; then
-    echo "ERROR:: Please run this script with root privelege"
-    exit 1 # failure is other than 0
-fi
 
 dnf install mysql -y
 
